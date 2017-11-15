b414a5be18fc6eeb5059ef5c2542c826125a7320
anik3tra0 <anik3tra0@gmail.com>
2017-11-15 12:16:51 +0530

    Validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ActiveRecord::Base
     has_many :users
+    validates :business_name, :subdomain, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
     belongs_to :account
+    validates :username, :email, uniqueness: true
 end
