e765c7aa7052622d9bf33b1134ded03a761e1e45
Rachit Aggarwal <rachit@syook.com>
2017-11-16 16:25:18 +0530

    CORS Headers is now fixed

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.3.1'
 
 gem 'rails', '4.2.8'
 gem 'rails-api'
+gem 'responders'
+gem 'rack-cors', require: 'rack/cors'
 
 group :development do
     gem 'spring'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :accounts, only: :create
-  resources :users, only: [:create, :update]
-  post '/login' => 'users#login'
+  resources :accounts, only: :create, defaults: { format: :json }
+  resources :users, only: [:create, :update], defaults: { format: :json }
+  post '/login' => 'users#login', defaults: { format: :json }
 end
