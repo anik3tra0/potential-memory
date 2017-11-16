c5b0c0e0708dbb1220f1b46281ae72d338ba5189
anik3tra0 <anik3tra0@gmail.com>
2017-11-16 18:10:33 +0530

    User Index API Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :accounts, only: :create, defaults: { format: :json }
-  resources :users, only: [:create, :update], defaults: { format: :json }
+  resources :users, only: [:create, :update, :index], defaults: { format: :json }
   post '/login' => 'users#login', defaults: { format: :json }
 end
